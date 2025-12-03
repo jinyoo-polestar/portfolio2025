1f55d7c82391029194c96d0afc5aa5b77f816a39
oooscorp <oooscorp@naver.com>
2025-12-03 22:21:35 +0900

    Design: apply footer ui design - pc/mo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import FooterImg from "../../public/img_footer.png";
+import ArrowIcon from "../../public/ic_arrow.svg";
 import "./Footer.scss";
 
 export default function Footer() {
@@ -15,20 +16,17 @@ export default function Footer() {
       </p>
       <div className="footer__info-box en-b2">
         <p className="footer__desc">
-          Code Code Code Code Code Code{" "}
-          <span className="footer__desc--highlight">
-            {" "}
-            Code Code Code Code Code Code,
-          </span>
-          Code Code Code Code Code Code{" "}
-          <span className="footer__desc--highlight">
-            {" "}
-            Code Code Code Code Code Code,
-          </span>
+          <span className="footer__desc--highlight">작은 디테일까지 </span>
+          놓치지 않고,
+          <br />
+          <span className="footer__desc--highlight">디자인의 결 </span>을 살리는
+          <span className="footer__desc--highlight"> 개발자</span>
         </p>
         <div className="footer__email">
           <span className="en-b2">sju9808@naver.com</span>
-          <a href="mailto:sju9808@naver.com"></a>
+          <a href="mailto:sju9808@naver.com">
+            <Image src={ArrowIcon} alt="contact" width={24} height={24} />
+          </a>
         </div>
         <small className="footer__copyright">jinyoo.2025</small>
       </div>
